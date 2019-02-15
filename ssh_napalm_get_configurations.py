f77aa024692d21772c05992f460da8b9a971216c
Anubhav <myster.anubhav@gmail.com>
2019-02-15 04:32:27 +0400

    initial commit

--- ssh_napalm_get_configurations.py
+++ ssh_napalm_get_configurations.py
@@ -10,39 +10,39 @@ IP_LIST = ["192.168.44.110", "192.168.44.111", "192.168.44.121", "192.168.44.122
 for IP in IP_LIST:
 
     DRIVER = get_network_driver("ios")
-    IOS_L2_S1 = DRIVER(str(IP), "anubhav", "cisco")
-    IOS_L2_S1.open()
+    DEVICE = DRIVER(str(IP), "anubhav", "cisco")
+    DEVICE.open()
 
     print("Connected to " + str(IP))
     # ------------------------------------
     print("Facts - " + str(IP))
-    OUTPUT = IOS_L2_S1.get_facts()
+    OUTPUT = DEVICE.get_facts()
     OUTPUT = json.dumps(OUTPUT, indent=4)
     print(OUTPUT)
     # ------------------------------------
     print("Interfaces - " + str(IP))
-    OUTPUT = IOS_L2_S1.get_interfaces()
+    OUTPUT = DEVICE.get_interfaces()
     OUTPUT = json.dumps(OUTPUT, sort_keys=True, indent=4)
     print(OUTPUT)
     # ------------------------------------
     print("Interfaces IP - " + str(IP))
-    OUTPUT = IOS_L2_S1.get_interfaces_ip()
+    OUTPUT = DEVICE.get_interfaces_ip()
     OUTPUT = json.dumps(OUTPUT, sort_keys=True, indent=4)
     print(OUTPUT)
     # ------------------------------------
     print("MAC Address Table - " + str(IP))
-    OUTPUT = IOS_L2_S1.get_mac_address_table()
+    OUTPUT = DEVICE.get_mac_address_table()
     OUTPUT = json.dumps(OUTPUT, sort_keys=True, indent=4)
     print(OUTPUT)
     # ------------------------------------
     print("ARP Table - " + str(IP))
-    OUTPUT = IOS_L2_S1.get_arp_table()
+    OUTPUT = DEVICE.get_arp_table()
     OUTPUT = json.dumps(OUTPUT, sort_keys=True, indent=4)
     print(OUTPUT)
     # ------------------------------------
     print("BGP Neighbors - " + str(IP))
-    OUTPUT = IOS_L2_S1.get_bgp_neighbors()
+    OUTPUT = DEVICE.get_bgp_neighbors()
     OUTPUT = json.dumps(OUTPUT, sort_keys=True, indent=4)
     print(OUTPUT)
 
-    IOS_L2_S1.close()
+    DEVICE.close()
